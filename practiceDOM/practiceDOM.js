80fb759ad8f4087758e19004c9c655d68220e355
Xiao Yu <xiao.susan.yu@gmail.com>
2019-12-12 15:06:00 +0800

    [Xiao]{feat}: modify practiceDOM

--- practiceDOM/practiceDOM.js
+++ practiceDOM/practiceDOM.js
@@ -2,10 +2,12 @@ function check() {
 var username = document.getElementById("username");
 var password = document.getElementById("password");
 var result = document.getElementById("result");
-  if ((username.value == "123") && (password.value == "123")) {
+  if ((username.value === "123") && (password.value === "123")) {
     result.innerHTML = "correct!";
+    result.style.color = "green";
   } else {
-    result.innerHTML = "wrong password or username";
+    result.innerHTML = "wrong password or username!";
+    result.style.color = "red";
     password.value = "";
   }
 }
